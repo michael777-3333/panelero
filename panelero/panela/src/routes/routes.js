50e0be1bb51c0320ebb4d3eed3b1eac45837dbc7
michael777-3333 <porrasmichael882@gmail.com>
2023-04-21 11:51:56 -0500

    ultimo push del dia viernes

--- panelero/panela/src/routes/routes.js
+++ panelero/panela/src/routes/routes.js
@@ -1,7 +1,7 @@
 import Login from "../components/Login.vue"
 import Body from "../components/Body.vue"
 import Home from "../components/modulos/Home.vue"
-import CostoModulos from "../components/modulos/cosModulos.vue"
+import CostosModulos from "../components/modulos/costos/CostosModulos.vue"
 import Usuario from '../components/modulos/mantenimiento/Usuario.vue'
 
 export const routes = [
@@ -13,7 +13,7 @@ export const routes = [
                 ]
             },
             { path:"costos", children: [
-                { path:"costoEtapa", component:CostoModulos}
+                { path:"costoModulos", component:CostosModulos}
             ]
             }
         ]
